4016e50b84ba0bc93b32d99aa738748c31f1a446
79511 <79511@LAPTOP-EJU4OS3A>
2023-08-04 19:27:16 +0700

    Добавьте файлы проекта.

---
+++ random/random.cpp
@@ -0,0 +1,31 @@
+﻿// random.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    srand(time(NULL));
+
+    int a, k = 0;
+
+    int x = rand() % 100;
+
+    do {
+        cout << "Введите число" << endl;
+        cin >> a;
+        if (a == x) cout << "Вы угадали" << endl;
+        if (a < x) cout << "Загаданное число больше" << endl;
+        if (a > x) cout << "Загаданное число меньше" << endl;
+        k++;
+    } while (a != x);
+    cout << "Количество попыток: " << k << endl;
+    return 0;
+}
+
+    
